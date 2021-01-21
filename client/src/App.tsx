f2b0277c67c6e1182fe837f1e1f97cf69996de4f
Larry Agbana <larryagbana@gmail.com>
2021-01-21 14:11:57 -0500

    Update app setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,41 @@
+import React from 'react';
 import './App.css';
 import {
   ApolloClient,
   ApolloProvider,
   NormalizedCacheObject,
 } from '@apollo/client';
-import { NavBar, Wrapper } from './components';
-import styled from 'styled-components';
+import { NavBar, Wrapper, ThemeContainer } from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Sales, Inventory, Participants } from './pages';
-interface appProps {
+import { ThemeProvider } from './store/ThemeProvider';
+
+interface AppProps {
   client: ApolloClient<NormalizedCacheObject>;
 }
 
-const App = ({ client }: appProps) => {
+const App = ({ client }: AppProps) => {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <NavBar />
-        <Wrapper>
-          <Switch>
-            <Route path="/inventory">
-              <Inventory />
-            </Route>
-            <Route path="/participants">
-              <Participants />
-            </Route>
-            <Route path={['/', '/sales']}>
-              <Sales />
-            </Route>
-          </Switch>
-        </Wrapper>
+        <ThemeProvider>
+          <NavBar />
+          <ThemeContainer>
+            <Wrapper>
+              <Switch>
+                <Route path="/inventory">
+                  <Inventory />
+                </Route>
+                <Route path="/participants">
+                  <Participants />
+                </Route>
+                <Route path={['/', '/sales']}>
+                  <Sales />
+                </Route>
+              </Switch>
+            </Wrapper>
+          </ThemeContainer>
+        </ThemeProvider>
       </Router>
     </ApolloProvider>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+// import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql', // TODO: Update to deployed URL
-  cache: new InMemoryCache()
+  uri: 'http://localhost:8080/graphql', // TODO: Update to deployed URL
+  cache: new InMemoryCache(),
 });
 
+// client.resetStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App client={ client}/>
+    <App client={client} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
