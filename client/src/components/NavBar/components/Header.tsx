236c6f873de76b94ee416171da973420b070c80a
Larry Agbana <larryagbana@gmail.com>
2021-01-21 14:14:33 -0500

    Update Header.tsx

--- client/src/components/NavBar/components/Header.tsx
+++ client/src/components/NavBar/components/Header.tsx
@@ -1,17 +1,18 @@
+import React from 'react';
 import styled from 'styled-components';
+import { useThemeContext } from '../../../store/useThemeContext';
 
 interface HeaderProps {}
-
 export const Header: React.FC<HeaderProps> = ({ children }) => {
-  let theme = 'dark';
-  const bkColor = theme === 'dark' ? 'blue' : 'white';
-  const color = theme === 'dark' ? 'white' : 'blue';
+  const {
+    state: { Navbar_COL, NavbarText_COL },
+  } = useThemeContext();
 
   const Header = styled.header`
     max-width: 100vw;
     height: 7vh;
-    background-color: ${bkColor};
-    color: ${color};
+    background-color: ${Navbar_COL};
+    color: ${NavbarText_COL};
   `;
 
   return <Header> {children}</Header>;
