9e69bcb8de6940b7d32b85cd629fd0a151709566
Larry Agbana <larryagbana@gmail.com>
2021-01-21 14:14:36 -0500

    Update NavBar.tsx

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,23 +1,39 @@
-import { Navigation, Header, NavContainer, Drawer } from './components';
-import { Wrapper } from '../Wrapper';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Navigation, Header, NavContainer, Drawer } from './components';
+import { Toggle, Wrapper } from '../../components';
 import logoForDark from '../../Logo-forDark.png';
 import logoForLight from '../../Logo-forLight.png';
+import { useThemeContext } from '../../store/useThemeContext';
+import { ActionTypes } from '../../store/Theme.interface';
+
 
 export const NavBar = () => {
-  let theme = 'dark';
-  const LogoImage = theme === 'dark' ? logoForDark : logoForLight;
+  const [isChecked, setIsChecked] = useState(true);
+  const { dispatch } = useThemeContext();
+  const LogoImage = isChecked ? logoForLight : logoForDark;
 
   return (
     <Header>
       <Wrapper>
         <NavContainer>
-          <a href="#">
+          <a href="/">
             <Img src={LogoImage} alt="fake nibbles logo" />
           </a>
           <Navigation open={false} />
           <div>
-            <Button href="/hire-me">Hire Me!</Button>
+            <Span>On</Span>
+            <Toggle
+              isChecked={isChecked}
+              onChecked={(e: React.ChangeEvent<HTMLInputElement>) => {
+                const checked = e.currentTarget.checked;
+                checked
+                  ? dispatch({ type: ActionTypes.Light })
+                  : dispatch({ type: ActionTypes.Dark });
+                setIsChecked(checked);
+              }}
+            />
+            <Span>Off</Span>
           </div>
           <Drawer />
         </NavContainer>
@@ -26,35 +42,17 @@ export const NavBar = () => {
   );
 };
 
-const Button = styled.a`
-  border-radius: 3px;
-  padding: 0.5rem 0.25rem;
-  width: 11rem;
-  color: white;
-  border: 2px solid white;
-  background: teal;
-  text-decoration: none;
-  &:hover {
-    background-color: green;
-  }
-
-  @media (max-width: 768px) {
-    margin-left: 15vw;
-    padding: 0.35rem 0.25rem;
-    font-size: 1rem;
-  }
-
-  @media (max-width: 280px) {
-    margin-left: 7vw;
-    padding: 0.25rem 0.25rem;
-    width: 8rem;
-    font-size: 0.8rem;
-  }
-`;
-
 const Img = styled.img`
   height: 6vh;
   max-width: 100%;
   margin-top: 0.5vh;
   margin-bottom: 0.5vh;
+`;
+
+const Span = styled.span`
+  padding: 4px;
+  
+  @media (max-width: 768px) {
+    font-size: 0.55rem;
+  };
 `;
