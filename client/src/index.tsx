3b7ccd0b9a7a10f83017f1d67aef567555b46ff3
Larry Agbana <larryagbana@gmail.com>
2021-01-21 18:18:24 -0500

    redeploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://nibbles-event.herokuapp.com/graphql', // TODO: Update to deployed URL
+  uri: 'https://nibbles-event.herokuapp.com/graphql',
   cache: new InMemoryCache(),
 });
 
