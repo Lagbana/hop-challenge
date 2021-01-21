e141f7d41f28ea31e4ce32e933d3ae80bfe0e475
Larry Agbana <larryagbana@gmail.com>
2021-01-21 14:08:53 -0500

    Create brand.tsx

---
+++ client/src/utils/brand.tsx
@@ -0,0 +1,20 @@
+/**
+ * Brand Colors
+*/
+export const Colors = {
+  blue: '#3183C8',
+  darkBlue: '#000066',
+  navyBlue: '#2269A1',
+  lightBlue: '#A9D5F5',
+  green: '#39C172',
+  red: '#DC3232',
+  yellow: '#FDB833',
+  darkGrey: '#617691',
+  midGrey: '#D5DDE9',
+  lightGrey: '#F5F7FA',
+  greyRGB: 'rgba(97, 118, 145, 1)',
+  lighGreyRGB: 'rgba(97, 118, 145, 0.3)',
+  white: '#FFFFFF',
+  offWhite: '#F8F8F8',
+  black: '#000000',
+};
